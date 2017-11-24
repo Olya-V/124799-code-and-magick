56475e598e2db34a772d46000e72a39fe2704f32
Olga <79081032777@mail.ru>
2017-11-24 17:16:24 +0600

    ctx v.4

--- js/stat.js
+++ js/stat.js
@@ -9,7 +9,7 @@ window.renderStatistics = function (ctx, names, times) {
 
 
   var connectPoints = function (arrayOfPoints, shift, сtx) {
-    сtx = сtx;
+    var сtx = сtx;
     arrayOfPoints = arrayOfPoints || [];
     shift = shift || 0;
     for (var i = 0; i <= arrayOfPoints.length - 1; i++) {
@@ -18,7 +18,7 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   var drawShape = function (color, startPoints, arrayOfPoints, shift, сtx) {
-    сtx = сtx;
+    var сtx = сtx;
     ctx.fillStyle = color || '#000000';
     startPoints = startPoints || [];
     arrayOfPoints = arrayOfPoints || [];
